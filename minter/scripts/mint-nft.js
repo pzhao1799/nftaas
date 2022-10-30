0a1a9c0c22886c0b803a02dc5542bc278c6fa1b1
healthyred <jhl3@williams.edu>
2022-10-30 10:43:38 -0400

    added safeMInt

--- minter/scripts/mint-nft.js
+++ minter/scripts/mint-nft.js
@@ -4,12 +4,26 @@ const NFT_NAME = "NFT"
 const CONTRACT_ADDRESS = "0x334CDe7E6Ab7bbdd7B9f76F1543d4741176bc4a1"
 const META_DATA_URL = "ipfs://bafyreiarrnhpwygfgr3oye2ytwybiri5sqsdys35vdgjbm266xf6n7kofu/metadata.json"
 
+const GIFT_CARD_NFT = "GiftCardNFT"
+const CARD_NFT_CONTRACT_ADDRESS = "0x069eEc21e749624ee9624b7A823b0bf7275fbcA0"
+
 async function mintNFT(contractName, contractAddress, metaDataURL, receiverAddress = null) {
     const contractFactory = await ethers.getContractFactory(contractName)
     const [owner] = await ethers.getSigners()
     receiver = receiverAddress ?? owner.address
     await contractFactory.attach(contractAddress).mintNFT(receiver, metaDataURL)
     console.log("NFT minted to: ", receiver)
+}
+
+async function safeMint(contractName, contractAddress, metaDataURL, value_amount, message, receiverAddress = null) {
+    const contractFactory = await ethers.getContractFactory(contractName)
+    console.log("hello")
+    const options = {value: ethers.utils.parseEther(value_amount)}
+    const [owner] = await ethers.getSigners()
+    receiver = receiverAddress ?? owner.address
+    console.log("got here")
+    await contractFactory.attach(contractAddress).safeMint(receiver, metaDataURL, message, owner.address, options)
+    console.log("NFT minted to: ", receiver, "with value: ", value_amount)
 }
 
 // mintNFT(NFT_NAME, CONTRACT_ADDRESS, META_DATA_URL)
@@ -19,4 +33,12 @@ async function mintNFT(contractName, contractAddress, metaDataURL, receiverAddre
 //        process.exit(1);
 //    });
 
-module.exports = { mintNFT }
+console.log("yop")
+safeMint(GIFT_CARD_NFT, CARD_NFT_CONTRACT_ADDRESS, META_DATA_URL, "0.1", "happyholidays")
+   .then(() => process.exit(0))
+   .catch((error) => {
+       console.error(error);
+       process.exit(1);
+   });
+
+module.exports = { mintNFT, safeMint }
